4a512a836c149e4938e05a56caf1b21297594a8a
eduard <eduard@cieep.com>
2022-11-11 17:33:29 +0100

    Initial commit

---
+++ src/com/cieep/ejercicio02_construir/Main.java
@@ -0,0 +1,19 @@
+package com.cieep.ejercicio02_construir;
+
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        StringBuilder frase = new StringBuilder();
+        String palabra;
+
+        while (!(palabra = scanner.nextLine()).equalsIgnoreCase("FIN")){
+            frase.append(palabra).append(" ");
+        }
+
+        System.out.println(frase.toString().trim());
+    }
+}
